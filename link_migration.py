b21dbae7ef36b5fed6f5905a620fd6b614970395
samuelnunoo <samuelnunoo@Mail.com>
2019-07-17 14:04:21 -0700

    completed

--- link_migration.py
+++ link_migration.py
@@ -1,52 +1,50 @@
-#https://openpyxl.readthedocs.io/en/stable/usage.html#read-an-existing-workbook
-
-
-'''from openpyxl import load_workbook
-
-wb = load_workbook(filename = 'mysql_tables.xlsx')
-posts = wb['wp_posts']
-'''
-
 
 #https://stackoverflow.com/questions/51800122/using-openpyxl-to-find-rows-that-contain-cell-with-specific-value-python-3-6
 import pandas as pd
 import re
+import csv
 
 base_url = 'https://www.hmc.edu/cis/'
 sql = pd.read_excel("mysql_tables.xlsx")
-links = pd.read_excel('links.xlsx')
 
 
 
 
-Link_Migrations = {}
+
+def Iterates():
+    with open ("links.csv",'r+') as f:
+        with open('file.txt','w+') as file:
+            reader = csv.reader(f, delimiter = '\n')
+
+            for content in reader:
+                content = content[0]
+                query = re.search(r"p=\d+", content)
+                if query:
+                    post_id = re.search(r"\d+", query.group()).group()
+
+                    if post_id:
+                        print("ok",post_id)
+                        extracted_date = sql[sql['ID'] == int(post_id)]['post_date'].values[0].__str__()
+
+                        extracted_date = re.match(r'\d{4}-\d{2}-\d{2}', extracted_date).group()
+                        new_date = re.sub('-', '/', extracted_date) + '/'
+                        slug = sql[sql['ID'] == int(post_id)]['post_name'].values[0].__str__()
+
+                        # New Url
+                        url = base_url + new_date + slug
+                        string = "Original| {content} | New | {url} \n".format(content=content, url=url)
+                        file.write(string)
 
 
-def Iterate():
-    with open ("links.txt",'w+') as f:
-        for index,row in links['Links'].iteritems():
-
-            query = re.search("?p=\d+",row).group()
-            post_id = re.search("\d+", query).group()
-
-            if post_id:
-
-                #Conversions
-                extracted_date = sql[sql['ID']== post_id]['post_date'].values[0].__str__()
-                extracted_date = re.match('\d{4}-\d{2}-\d{2}',extracted_date).group()
-                new_date = re.sub('-','/',extracted_date) + '/'
-                slug = sql[sql['ID'] == post_id]['post_name'].values[0]
 
 
-                #New Url
-                url = base_url + new_date + slug
+              
 
-                f.write("Original| {0} | New | {1} ").format(row,url)
+
+
 
     f.close()
-
-    
-
+    file.close()
 
 
 
@@ -54,6 +52,9 @@ def Iterate():
 
 
 
+
+
+Iterates()
 
 
 
